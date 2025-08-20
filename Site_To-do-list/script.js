bcb80542aadfc3e338eabddc2b7d29fb1e468887
Cleiton Queiroz <125406583+TonQueiroz@users.noreply.github.com>
2025-08-19 22:36:35 -0300

    Commit Após melhorias
    melhorias de visual, conteúdo e interação

--- Site_To-do-list/script.js
+++ Site_To-do-list/script.js
@@ -1,22 +1,70 @@
-function AdicionarTarefa(){   
 
+
+//Função
+function ValidarDadosVazios(dadoValidado){
+    if (dadoValidado === '') {        
+        window.alert('Insira um dado Válido.')
+        throw new Error("Sem Dados");
+        
+    }}
+
+let tarefaEscrita = document.querySelector('input#cxEntrada')
+
+//Adicionando conteúdo com Enter
+tarefaEscrita.addEventListener('keydown', function(tecla){
+if(tecla.key === 'Enter'){   
+    AdicionarTarefa()
+}
+})
+
+//Função
+function AdicionarTarefa(){
+    
+
+    ValidarDadosVazios(tarefaEscrita.value)
+    
     let sectionlistaDeTarefas = document.querySelector('section#tarefas')
     sectionlistaDeTarefas.style.display = 'Flex' //Deixei a lista de tarefas visível
-
-    let tarefaEscrita = document.querySelector('input#cxEntrada').value //selecionei o que foi escrito
+    
 
     let itemlista = document.createElement('li')//Criação do Item 
 
-    itemlista.innerHTML = '✒️ -  '+ tarefaEscrita +'<span onclick="DeletarTarefa(this)">✅</span>' //coloquei o que foi escrito no item 
+    itemlista.innerHTML = '✒️ -  '+ tarefaEscrita.value +' <span class="xcheck"onclick="ConcluirTarefa(this)">✅</span> <span class="xdelete" onclick="DeletarTarefaUnica(this)">❌</span>' //coloquei o que foi escrito no item 
     // O this dentro da chamada da função faz o a hora que eu clicar ele apontar para ele mesmo aí ele vai excluir ele mesmo atravez do parentRemove(remoção do pai) na função
 
-    let listadeTarefas = document.querySelector('ul') //selecionei a lista
+    let listadeTarefas = document.querySelector('ul#tarefasAFazer') //selecionei a lista
     listadeTarefas.appendChild(itemlista) //adicionei o item a ul(lista)
 
-    document.querySelector('input#cxEntrada').value = ''    
-    
+    document.querySelector('input#cxEntrada').value = '' 
 }
 
-function DeletarTarefa(tagSpanTatefa){
-        tagSpanTatefa.parentElement.remove() //parenteElement Seleciona o pai do span(tagSpanTatefa) que é o Item da lista e o remove exclui ele;
+
+//Função
+function ConcluirTarefa(tagSpanTatefa){ 
+    let tarefaConcluída = tagSpanTatefa.parentElement
+
+    let listaConcluida = document.querySelector('ul#tarefasConcluídas')
+
+    listaConcluida.style.display = 'block'
+
+    listaConcluida.appendChild(tarefaConcluída)
+
+    console.log(tarefaConcluída)
+
 }
+
+//Função
+function DeletarTarefas(tagSpanTatefa){
+    let listaconclida = document.querySelector('ul#tarefasConcluídas')
+    listaconclida.innerHTML=''
+
+    let listaConcluida = document.querySelector('ul#tarefasConcluídas')
+    listaConcluida.style.display = 'none'
+
+}
+
+//Função
+function DeletarTarefaUnica(tagSpanTatefa){
+    tagSpanTatefa.parentElement.remove()
+}
+
